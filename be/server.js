b71d25ea7aaac3d2940aa22a2483fd530898a26f
fan <fannanff25@gmail.com>
2025-05-08 20:16:36 +0700

    :hammer: adding cors

--- be/server.js
+++ be/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const dontenv = require("dotenv").config();
 const connectDb = require("./config/connectionDB");
-
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 connectDb();
 app.use(express.json());
+app.use(cors());
 
 app.use("/recipe", require("./routes/recipe"));
 
